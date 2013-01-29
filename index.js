654c4144ba4e21a8462b95ddcff25e8f42721e07
mafintosh <mathiasbuus@gmail.com>
2013-01-29 20:35:48 +0100

    null typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var INDENT = '    ';
 
 var type = function(doc) {
-	if (doc === 'null') return 'null';
+	if (doc === null) return 'null';
 	if (Array.isArray(doc)) return 'array';
 	if (typeof doc === 'string' && /https?:/.test(doc)) return 'link';
 
